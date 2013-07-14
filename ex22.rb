9c4b04217da1f2d7339fba4b74cf76a1fbc7f62a
Richard Howard <richardahoward@gmail.com>
2013-07-13 20:01:58 -0700

    some things I know, I gots to learn MOR

---
+++ ex22.rb
@@ -0,0 +1,46 @@
+# So this lesson blows the big fat one.
+# I mean it really blows
+# It blows like a whale getting a much needed breath
+
+
+# this is a comment intended to document what code is doing
+
+# Math is next
+
++ # this is a plus sign for adding
+= # this is a equals sign for sumingthings up
+/ # this is a dividing symbol
+* # this is a multiplication symbol
+- # this is a subtrction symbol
+** # exponential, this is for using the concept of exponential numbers
+% # this is modulo, I fucking hate this stupid fucking symbol. it divides the left by the right and return a remainder. Fuck you modulo, fuck you.
+
+
++= # this will take the current variable on the left and add the variable on the right to the first variable
+/= # this will take the current variable on the left and divide it by the variable on the right
+-= # this will take the current variable on the left and subtract the variabel on the right from it
+*= # this will take the current variable on the left and multiply it by the variable on the right
+%= # this is the same as above but using modulo. Again, fuck you modulo
+**= # exponential power mutation for original variable
+
+# comparisons.... sigh. aka true or false
+
+==  # a true or false return, if a = and b = 1 does a == b ? yes, yes it does
+!=  # the exact fucking opposite from the above
+>   # is the left greater then the right?
+<   # is the right greater then the left?
+>=  # is the left greater or equal to the right?
+<=  # is the right greater or equal to the left?
+<=> # this is space ship. this thing returns -1 if right is greater 0 if left and right are equal and 1 if left is greater
+
+# Some other stuff
+
+# puts  # this is to return a string to the cli or to the web form
+# print # print this thing out
+# def   # defines a function
+# end   # ends a method of other function
+# if    # portion of the if this then that comparitive 
+# elsif # portion ""
+# else  # portion ""
+# 
+
